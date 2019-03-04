8335dade988635424faad45ef51049a4f9cf9e2e
Collin Webb <collindwebb@gmail.com>
2019-03-04 11:20:23 -0800

    Add more detailed comments & less detailed logging

--- explorations.py
+++ explorations.py
@@ -1,16 +1,25 @@
+# -*- coding: utf-8 -*-
+
 """
-write things!!
+explorations.py is a short set of python scripts which currently compare two sets
+of brain wave recordings and show the difference between them.
+
+In a more finilized version, one would upload any neural oscillation data set(s)
+and analyze them in a few ways.
 """
 
-from mne import Epochs, find_events
+from mne import Epochs, find_events, set_log_level
 from mne.io import read_raw_edf
+
+# set logging level
+set_log_level(verbose="WARNING")
 
 ###############
 # Import Data #
 ###############
 
 # Define Paths
-# TODO: automate based on brefix names (with muse prefixes are before the underscore).
+# TODO: automate based on brefix names (with muse, prefixes are before the underscore)
 filePath = "MuseData/"
 beforePracticeFile = filePath + "Before Mindfulness_Muse-BE53_2019-01-26--15-44-02_1548546368888.edf"
 afterPracticeFile = filePath + "Post Muse 5 min v4.2_Muse-BE53_2019-01-26--15-56-03_1548547065702.edf"
@@ -19,12 +28,10 @@ afterPracticeFile = filePath + "Post Muse 5 min v4.2_Muse-BE53_2019-01-26--15-56
 # Read Raw Data
 # TODO 1: This should be all that is needed. There is a chance I need to mess with the
 # channels or annotate the data to get at least more detailed readings.
-# TODO 2 automate based on Path automation above.
+# TODO 2 automate based on Path automation above and add it to the class.
 
 beforePracticeRawData = read_raw_edf(beforePracticeFile, stim_channel="auto", preload=True)
 afterPracticeRawData = read_raw_edf(afterPracticeFile, stim_channel="auto", preload=True)
-
-# this is just for beforePracticeRawData
 
 #############
 # Read Data #
@@ -32,15 +39,26 @@ afterPracticeRawData = read_raw_edf(afterPracticeFile, stim_channel="auto", prel
 
 class BasicBrainData(object):
     """
-    description TODO: write
+    Description:
+        This is a basic class whose purpose is to look at electrical impulses from
+        the brain and do basic analytics to it
 
-    Attributes (two raw edf files to compare):
-        before: edf file from readings *before* some event
-        after: edf file from readings *after* some event
+    Attributes:
+        Editable (two raw edf files to compare):
+            before: edf file from readings *before* some event
+            after: edf file from readings *after* some event
+        Static:
+            bands: the frequeny bands most neural oscillations are catagorized into
+        Returned:
+            averagedData:
+                data that has been flattened and averaged to see in which bands the
+                most neural oscillations happened in
     """
 
+    # TODO: make more flexable for other kinds of comparison and explorations
+    #       e.g. delete before and after and allow for any amount of annotated data
     def __init__(self, before, after):
-        """Returns a BasicBrainData Object with before and after attributes"""
+        """Returns a BasicBrainData Object with provided before & after attributes"""
         self.before = before.pick_types(eeg=True, exclude='bads')
         self.after = after.pick_types(eeg=True, exclude='bads')
         #define brain wave bands
@@ -51,32 +69,36 @@ class BasicBrainData(object):
             (12, 30, "Beta"),
             (30, 100, "Gamma")
         ]
-
-    def getAveragesOverBrainWaves(self, data):
         # initialize dictionary
         self.averagedData = {
-        "Delta": 0,
-        "Theta": 0,
-        "Alpha": 0,
-        "Beta": 0,
-        "Gamma": 0
+            "Delta": 0,
+            "Theta": 0,
+            "Alpha": 0,
+            "Beta": 0,
+            "Gamma": 0
         }
 
-        # iterate over bands to find the average volts per band over the whole time
+    def getAveragesOverBrainWaves(self, data):
+        """iterate over bands to find the average volts per band over time."""
+        # TODO 1: make sure the data has been correctly preprocessed.
+        # TODO 2: evaluate this agaist known good analysis.
+        # TODO 3: try a version with this split up into 1hz bands from .5 to 100.5
+        #         and see if the Delta, Theta, etc bands make sense as constructs.
         for fmin, fmax, name in self.bands:
-            filteredData = data.filter(fmin, fmax)
+            filteredData = data.filter(fmin, fmax, verbose=None)
             flattenedData = [y for x in filteredData.get_data() for y in x]
             average = sum(flattenedData) / float(len(flattenedData))
             self.averagedData[name] = average
 
         return self.averagedData
 
-
-    def compare(self):
+    def compareBeforeAfter(self):
+        """compares two data sets and returns data plus diffrenes"""
         beforeDataAveraged = self.getAveragesOverBrainWaves(self.before)
         afterDataAveraged = self.getAveragesOverBrainWaves(self.after)
+        #MAKE DIFFERENCES
         print(beforeDataAveraged, afterDataAveraged)
 
 
 mindfulnessData = BasicBrainData(beforePracticeRawData, afterPracticeRawData)
-mindfulnessData.compare()
+mindfulnessData.compareBeforeAfter()
